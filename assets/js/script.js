8a7ca7811a511c3e6ffb82e044807960d93f03e7
Akash Ahirwar <101441960+akash-ahirwar02@users.noreply.github.com>
2023-05-27 10:25:35 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// toggle element
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navCloseBtn, navOpenBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+
+// header navbar
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+// go to top
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+
+})
